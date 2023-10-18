6b1ec7438662e3a1dca47e68df80679016ac68cc
minsun309 <pminsun309@gmail.com>
2023-10-18 09:55:13 +0900

    [Add] itemDetail 하단에 다음 / 이전 포스트 링크

--- src/components/item/index.tsx
+++ src/components/item/index.tsx
@@ -11,12 +11,9 @@ export default function Item({ item, viewStyle, tagCategory }: any) {
 
   const router = useRouter();
   const pathName =
-    router.pathname === "/blog" || "/"
-      ? `/blog/${item.id}`
-      : `/project/${item.id}`;
+    router.pathname === "/project" ? `/project/${item.id}` : `/blog/${item.id}`;
 
   const tagName = itemData.tags.map((row: any) => row.name);
-
   const categoryView =
     tagCategory === "all" ||
     tagName.includes(tagCategory) ||
--- src/pages/blog/[...id].tsx
+++ src/pages/blog/[...id].tsx
@@ -2,21 +2,54 @@
 import ItemDetailContent from "@/components/item/itemDetailContent";
 import axios from "axios";
 import { useRouter } from "next/router";
-import { HiArrowLeft } from "react-icons/hi";
+import {
+  HiArrowLeft,
+  HiOutlineArrowCircleLeft,
+  HiOutlineArrowCircleRight,
+} from "react-icons/hi";
 import { NextPageContext } from "next";
 import UseProperties from "libs/useProperties";
 import { BASE_URL, TOKEN } from "libs/config";
 import ItemDetailProp from "@/components/item/itemDetailProp";
 import Seo from "@/components/seo";
 import MoveToTop from "@/components/moveToTop";
+import Link from "next/link";
 
-export default function blockDetail({ blockDetail, propertiesData }: any) {
+export default function blockDetail({
+  blockDetail,
+  propertiesData,
+  tableData,
+}: any) {
   const router = useRouter();
   const backClick = () => {
     router.push("/blog");
   };
 
   const itemData = UseProperties(propertiesData);
+
+  // 이전, 다음 글
+  const pageInfo = (cell: any) => {
+    return { title: cell.plain_text, href: cell.href };
+  };
+
+  const pageInfoAll = (tableData: any, index: number) =>
+    tableData
+      .flatMap((table: any) => table.table_row?.cells[index])
+      .map(pageInfo)
+      .map((item: any) => ({ title: item.title, href: item.href }));
+
+  const preData = pageInfoAll(tableData.results, 0);
+  const nextData = pageInfoAll(tableData.results, 1);
+
+  const extractHrefID = (href: string) =>
+    href?.substring(href.lastIndexOf("-") + 1 || href.lastIndexOf("/") + 1);
+
+  // title & url
+  const preTitle = preData[0]?.title;
+  const nextTitle = nextData[0]?.title;
+  const preHref = extractHrefID(preData[0]?.href);
+  const nextHref = extractHrefID(nextData[0]?.href);
+
   return (
     <>
       <Seo
@@ -44,6 +77,36 @@ export default function blockDetail({ blockDetail, propertiesData }: any) {
             blockContent={blockContent}
           />
         ))}
+      </div>
+      <div className="flex items-center justify-between py-16 text-xs border-t-4 border-stone-200 border-dotted dark:border-stone-500">
+        {preHref && (
+          <Link
+            href={`/blog/${preHref}`}
+            className="bg-gray-100 rounded flex items-center px-2 py-3 group"
+          >
+            <HiOutlineArrowCircleLeft className="text-2xl text-gray-400 group-hover:text-[#2c82f2] mt-1" />
+            <div className="ml-4">
+              <p className="text-[10px] mb-1">이전 포스트</p>
+              <p className="font-bold w-48 whitespace-nowrap overflow-hidden text-ellipsis">
+                {preTitle}
+              </p>
+            </div>
+          </Link>
+        )}
+        {nextHref && (
+          <Link
+            href={`/blog/${nextHref}`}
+            className="flex items-center bg-gray-100 rounded px-4 py-3 ml-auto group"
+          >
+            <div className="mr-4 flex flex-col items-end">
+              <p className="text-[10px] mb-1">다음 포스트</p>
+              <p className="font-bold w-48 whitespace-nowrap overflow-hidden text-ellipsis text-right">
+                {nextTitle}
+              </p>
+            </div>
+            <HiOutlineArrowCircleRight className="text-2xl text-gray-400 group-hover:text-[#2c82f2] mt-1" />
+          </Link>
+        )}
       </div>
       <MoveToTop />
     </>
@@ -78,7 +141,24 @@ export async function getServerSideProps(context: NextPageContext) {
   const blockDetail = blockResponse.data;
   const propertiesData = propertiesResponse.data;
 
+  // table id 추출
+  let tableId = null;
+  blockDetail.results.map(
+    (blockContent: any) =>
+      blockContent.type === "table" && (tableId = blockContent.id)
+  );
+
+  // table_row data
+  let tableData = null;
+  if (tableId) {
+    const tableResponse = await axios.get(
+      `https://api.notion.com/v1/blocks/${tableId}/children`,
+      axiosConfig
+    );
+    tableData = tableResponse.data;
+  }
+
   return {
-    props: { blockDetail, propertiesData },
+    props: { blockDetail, propertiesData, tableData },
   };
 }
