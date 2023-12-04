fbfcc674b983e1fb8a2df4fa364a43bd4039261d
minsun309 <pminsun309@gmail.com>
2023-12-04 22:26:10 +0900

    [Add] Header style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ import { cls } from "libs/utils";
 export default function Header() {
   const router = useRouter();
   return (
-    <header className="fixed top-0 z-50 bg-primary w-full">
+    <header className="fixed top-0 z-50 w-full backdrop-blur-md bg-[#fbfbfa]/30">
       <div className="max-w-3xl flex items-center justify-between py-2 px-4 lg:px-0 mx-auto">
         <nav className="flex gap-2 items-start header-style">
           <Link
