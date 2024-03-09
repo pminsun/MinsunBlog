bbf956172b1d908627f197612a0b1cd4e88c9c61
minsun309 <pminsun309@gmail.com>
2024-03-09 22:31:50 +0900

    [Fix] layout tag 변경

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,13 +5,13 @@ import Footer from "./ScreenStructure/footer";
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <>
-      <main className="flex flex-col items-center bg-primary min-h-full relative pb-[65px]">
+      <section className="flex flex-col items-center bg-primary min-h-full relative pb-[65px]">
         <Header />
         <section className="w-full h-full py-4 lg:px-0 mt-[56px]">
           {children}
         </section>
         <Footer />
-      </main>
+      </section>
     </>
   );
 }
