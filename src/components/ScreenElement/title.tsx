b5604f07f50bf719b99755fda0a52c65a2b21c79
minsun309 <pminsun309@gmail.com>
2024-07-09 15:46:10 +0900

    [fix] Title 컴포넌트정리

--- src/components/ScreenElement/title.tsx
+++ src/components/ScreenElement/title.tsx
@@ -5,11 +5,9 @@ interface TitleType {
 
 export default function Title({ title, subMent }: TitleType) {
   return (
-    <div className="text-center lg:text-left">
-      <h2 className="text-2xl lg:text-3xl font-bold mb-0 lg:mb-2">{title}</h2>
-      <span className="text-[10px] lg:text-xs dark:text-slate-300">
-        {subMent}
-      </span>
+    <div className="title-container">
+      <h2>{title}</h2>
+      <span>{subMent}</span>
     </div>
   )
 }
